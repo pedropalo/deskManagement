b584bbcf5403d9e33dc18dd77a1e874cbb696171
Pedro Paulo <ped-cta@hotmail.com>
2018-10-08 00:43:58 -0300

    Melhorando leitura do código

--- deskManagement/formInitial.cs
+++ deskManagement/formInitial.cs
@@ -15,7 +15,7 @@ namespace deskManagement
 {
     public partial class formInitial : Form
     {
-
+        //Global
         private string _filePath = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
 
         public formInitial()
@@ -23,28 +23,14 @@ namespace deskManagement
             InitializeComponent();
         }
 
+        //Metodos compartilhados
         public void filePath()
         {
             _filePath = Directory.GetParent(_filePath).FullName;
             _filePath = Directory.GetParent(Directory.GetParent(_filePath).FullName).FullName;
         }
 
-        private void btnFechar_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void btnMinimizar_Click(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Minimized;
-        }
-
-        private void formInitial_Load(object sender, EventArgs e)
-        {
-            panelLateral.Height = this.Height;
-        }
-
-        public void abrirMenu( Form menu )
+        public void abrirMenu(Form menu)
         {
             panelContent.Controls.Clear();
 
@@ -55,11 +41,6 @@ namespace deskManagement
                 panelContent.Controls.Add(menu);
                 menu.Show();
             }
-        }
-
-        private void lblTeste_Click(object sender, EventArgs e)
-        {
-            abrirMenu(new contents.formConfig());
         }
 
         public void alteraCor(Label sender, bool reset)
@@ -75,6 +56,27 @@ namespace deskManagement
                     sender.ForeColor = Color.Red;
                 }
             }
+        }
+
+        //Demais metodos
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void formInitial_Load(object sender, EventArgs e)
+        {
+            panelLateral.Height = this.Height;
+        }
+
+        private void lblTeste_Click(object sender, EventArgs e)
+        {
+            abrirMenu(new contents.formConfig());
         }
 
         private void lblTeste_MouseEnter(object sender, EventArgs e)
